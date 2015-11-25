c4ce39e9e359c8f1e33b039f14303d9bc58f0d80
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-11-25 22:42:34 +0000

    minor mod aquireData.R

--- aquireData.R
+++ aquireData.R
@@ -1,9 +1,9 @@
 library(quantmod)
 
-# Grab these symbols from Yahoo
-syms <- c("^FTSE", "^GSPC", "^GDAXI", "^NDX")
+# List of Yahoo index symbols
+syms <- c("^FTSE", "^GSPC", "^NDX", "^N225", "^MERV")
 
-# Get the data
+# Grab the data from Yahoo
 rawInd <- lapply(syms, getSymbols, auto.assign=FALSE)
 
 # Just the close
@@ -12,5 +12,5 @@ rawClose <- lapply(rawInd, '[', i=, j=4)
 # Merge together to one time series
 allClose <- do.call("merge", rawClose)
 
+# Write out the data
 write.zoo(allClose, file="data/allClose.csv")
-
