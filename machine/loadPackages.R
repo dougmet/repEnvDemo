8273d2a69357613004c7ff17479a18002bc66029
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-11-26 21:31:43 +0000

    switchr mod

--- machine/loadPackages.R
+++ machine/loadPackages.R
@@ -1,10 +1,5 @@
 # Loading in the manifest
 library(switchr)
-libMan <- loadManifest("/tmp/packages.rman")
-#install_packages(libMan)
+libMan <- loadManifest("machine/packages.rman")
 
-# switchr not behaving as expected so just extract the manifest
-pkgs <- libMan@pkg_versions$name
-
-# Because we have a locked MRAN the versions will work here
-install.packages(pkgs)
+switchTo("repenv", seed=libMan)
