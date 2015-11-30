a51a62d954a0e47ede4c9f4a845d0bd8498a8187
Douglas Ashton <douglas.j.ashton@gmail.com>
2015-11-30 10:40:33 +0000

    Switched to pkgsnap

--- machine/savePackages.R
+++ machine/savePackages.R
@@ -1,4 +1,3 @@
 # Saving the manifest
-library(switchr)
-libMan <- libManifest(record_versions = TRUE)
-publishManifest(libMan, dest="machine/packages.rman")
+library(pkgsnap)
+snap("~/host/packages.csv")
